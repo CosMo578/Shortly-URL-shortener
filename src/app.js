8478356525ccce7346ea2995d82e17d2c86fe1ae
Dev. Ralph <raphaelakpor00@gmail.com>
2023-12-17 19:10:46 +0100

    updated design  ✨

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 // async function callApi(e) {
-//   // Prevent the default form submission behavior
+  // Prevent the default form submission behavior
 //   e.preventDefault();
 
-//   // Get the input element by its id
+  // Get the input element by its id
 //   let inputField = document.querySelector('#input');
-//   // let inputFieldValue = inputField.value;
+  // let inputFieldValue = inputField.value;
 
 //   let inputFieldValue = inputField.value.trim();
-//   // if (inputFieldValue === null) return;
+  // if (inputFieldValue === null) return;
 
 //   fetch(`https://api.shrtco.de/v2/shorten?url=${inputFieldValue}`)
 //     .then((response) => response.json())
 //     .then((response) => {
-//         // let shortlyCode = response.result.code;
+        // let shortlyCode = response.result.code;
 //         console.log(response);
 //     });
 // }
