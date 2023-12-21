fedaf63bedb211bf1c5fc26aef69095a9829539a
Dev. Ralph <raphaelakpor00@gmail.com>
2023-12-21 13:59:42 +0100

    Features and Styles Updated

--- src/app.js
+++ src/app.js
@@ -1,41 +1,7 @@
-const storedLinks = [];
 let toggler = document.querySelector('#menu');
 let clearButton = document.querySelector('.clear');
 let parentElement = document.querySelector('.links');
-let hasFunction1Run = false;
-
-const updateData = () => {
-  document.addEventListener('DOMContentLoaded', () => {
-    let fetchedLinks = getLocalData();
-    renderData(fetchedLinks);
-
-    if (fetchedLinks.length === 0) {
-      clearButton.style.display = 'none';
-    } else {
-      clearButton.style.display = 'block';
-    }
-  });
-};
-
-function getLocalData() {
-  const storedData = localStorage.getItem('Links');
-  return JSON.parse(storedData);
-}
-
-function renderData(links) {
-  if (links === null) {
-    return;
-  } else {
-    links.forEach((link) => {
-      const newLink = document.createElement('li');
-      newLink.innerHTML = `${link}`;
-
-      const linksParent = document.querySelector('.links');
-      linksParent.appendChild(newLink);
-    });
-  }
-}
-updateData();
+let eleArr = [];
 
 function validateUrl(url) {
   const regex =
@@ -43,48 +9,11 @@ function validateUrl(url) {
   return regex.test(url);
 }
 
-function showMenu() {
-  let nav = document.querySelector('nav');
-  if (nav.style.display != 'block') {
-    nav.style.display = 'block';
-    toggler.name = `close-circle-outline`;
-  } else {
-    nav.style.display = 'none';
-    toggler.name = `menu-outline`;
-  }
-}
-toggler.addEventListener('click', showMenu);
-
-function clearEle() {
-  storedLinks.length = 0;
-  localStorage.clear();
-  parentElement.replaceChildren();
-  clearButton.style.display = 'none';
-}
-clearButton.addEventListener('click', clearEle);
-
-function initializeClipboard() {
-  let clipboard = new ClipboardJS('.copyBtn');
-
-  clipboard.on('success', function (e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-  });
-
-  clipboard.on('error', function (e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-  });
-}
-
 async function callApi(e) {
   e.preventDefault();
 
   let inputField = document.querySelector('#input');
   let inputFieldValue = inputField.value;
-  const linksParent = document.querySelector('.links');
 
   if (!validateUrl(inputFieldValue)) {
     alert('Invalid URL. Please enter a valid web address.');
@@ -113,40 +42,125 @@ async function callApi(e) {
 
     const result = await response.json();
 
+    let eleId = Math.round(Math.random() * 100000);
+
     let dataText = `${inputFieldValue}<span id="short-url">${result.result_url}<button data-clipboard-text=${result.result_url} class="copyBtn copy">Copy</button></span>`;
 
     const newLink = document.createElement('li');
+    newLink.id = eleId;
     newLink.innerHTML = dataText;
+    parentElement.appendChild(newLink);
 
-    linksParent.appendChild(newLink);
+    eleArr.push({ dataText: dataText, eleId: eleId });
+    localStorage.setItem('Links', JSON.stringify(eleArr));
 
-    initializeClipboard();
-
-    storedLinks.push(dataText);
-    localStorage.setItem('Links', JSON.stringify(storedLinks));
     inputField.value = '';
   } catch (error) {
     alert(error.message);
   }
-  hasFunction1Run = true;
-
-  clearButton.style.display = 'block';
+  showClearBtn();
+  console.log(eleArr);
 }
 
-// document.addEventListener('DOMContentLoaded', () => {
-//
-// });
+function showMenu() {
+  let nav = document.querySelector('nav');
+  if (nav.style.display != 'block') {
+    nav.style.display = 'block';
+    toggler.name = `close-circle-outline`;
+  } else {
+    nav.style.display = 'none';
+    toggler.name = `menu-outline`;
+  }
+}
+toggler.addEventListener('click', showMenu);
 
-// if (hasFunction1Run) {
-//   let copyVal = document.querySelector('button.copyBtn');
-//   function copyText() {
-//     if (copyVal.className.includes('copy')) {
-//       copyVal.classList.remove('copy');
-//       copyVal.classList.add('copied');
-//       copyVal.innerHTML = 'Copied';
-//     }
-//   }
-//   if (copyVal) {
-//     copyVal.addEventListener('click', copyText);
-//   }
-// }
+function showClearBtn() {
+  if (parentElement.children.length > 0) {
+    let copyBtn = document.querySelector('.copyBtn');
+
+    function initializeClipboard() {
+      let clipboardButtons = document.querySelectorAll('.copyBtn');
+
+      clipboardButtons.forEach((copyBtn) => {
+        let clipboard = new ClipboardJS(copyBtn);
+
+        clipboard.on('success', function (e) {
+          console.info('Action:', e.action);
+          console.info('Text:', e.text);
+          console.info('Trigger:', e.trigger);
+
+          function copyText() {
+            if (copyBtn.className.includes('copy')) {
+              copyBtn.classList.remove('copy');
+              copyBtn.classList.add('copied');
+              copyBtn.innerHTML = 'Copied';
+            }
+          }
+          copyText();
+        });
+
+        clipboard.on('error', function (e) {
+          console.info('Action:', e.action);
+          console.info('Text:', e.text);
+          console.info('Trigger:', e.trigger);
+        });
+      })
+    }
+
+    initializeClipboard();
+    clearButton.style.display = 'block';
+  } else {
+    clearButton.style.display = 'none';
+  }
+}
+showClearBtn();
+
+function clearList() {
+  localStorage.removeItem('Links');
+  parentElement.replaceChildren();
+  eleArr.length = 0;
+  clearButton.style.display = 'none';
+}
+clearButton.addEventListener('click', clearList);
+
+function getLocalData() {
+  const storedData = localStorage.getItem('Links');
+  return JSON.parse(storedData);
+}
+
+function renderData(links) {
+  links.forEach((link) => {
+    const newLink = document.createElement('li');
+    newLink.id = link.eleId;
+    newLink.innerHTML = link.dataText;
+    parentElement.appendChild(newLink);
+  });
+  showClearBtn();
+}
+
+function updateData() {
+  if (localStorage.getItem('Links') !== null) {
+    let fetchedLinks = JSON.parse(localStorage.getItem('Links'));
+    renderData(fetchedLinks);
+  }
+  showClearBtn();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  updateData();
+  console.log(eleArr);
+});
+
+// // if (hasFunction1Run) {
+// //   let copyVal = document.querySelector('button.copyBtn');
+// //   function copyText() {
+// //     if (copyVal.className.includes('copy')) {
+// //       copyVal.classList.remove('copy');
+// //       copyVal.classList.add('copied');
+// //       copyVal.innerHTML = 'Copied';
+// //     }
+// //   }
+// //   if (copyVal) {
+// //     copyVal.addEventListener('click', copyText);
+// //   }
+// // }
